621405e4842f31f9bc1a2d4b4804c1ee03c02b65
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-11-24 11:56:09 -0300

    feat: user list with map

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -25,12 +25,23 @@ import { Sidebar } from "../../components/Sidebar";
 export default function UserList() {
   const { data, isLoading, error } = useQuery("users", async () => {
     const response = await fetch("http://localhost:3000/api/users");
-    const data = response.json();
+    const data = await response.json();
 
-    return data;
+    const users = data.users.map((user: any) => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }),
+      };
+    });
+
+    return users;
   });
-
-  console.log(data);
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -84,48 +95,24 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Kayke Fujinaka</Text>
-                        <Text fontSize="sm" color="gray.300">
-                          kaykealvesfujinaka@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>14 de novembro, 2022</Td>}
-                  </Tr>
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Kayke Fujinaka</Text>
-                        <Text fontSize="sm" color="gray.300">
-                          kaykealvesfujinaka@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>14 de novembro, 2022</Td>}
-                  </Tr>
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Kayke Fujinaka</Text>
-                        <Text fontSize="sm" color="gray.300">
-                          kaykealvesfujinaka@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>14 de novembro, 2022</Td>}
-                  </Tr>
+                  {data.map((user: any) => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={["4", "4", "6"]}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">
+                              {user.email}
+                            </Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                      </Tr>
+                    );
+                  })}
                 </Tbody>
               </Table>
 
